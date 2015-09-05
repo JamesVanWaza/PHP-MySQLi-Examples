2462483155831732a1c09f8ee5419046798ca41c
James Van Waza <jgacuca@gmail.com>
2015-09-05 12:31:14 -0400

    Code cleanup

--- checkboxes/nav.php
+++ checkboxes/nav.php
@@ -1,17 +1,6 @@
-<nav class="top-bar" data-topbar role="navigation" data-options="is_hover: false">
-    <ul class="title-area">
-        <li class="name">
-            <h1><a href="index.php">Checkboxes</a></h1>
-        </li>
-        <li class="toggle-topbar menu-icon"><a href="#"><span></span></a></li>
+<nav>
+    <ul>
+        <li><h1 class="title"><a href="checkboxes.php"></a>Checkboxes</h1></li>
+        <li><a href="#">Menu 1</a></li>
     </ul>
-    <section class="top-bar-section">
-        <!-- Right Nav Section -->
-        <ul class="right">
-            <li><a href="about.php" title="About Us">About Us</a></li>
-            <li><a href="faqs.php" title="Frequently Asked Questions">FAQs</a></li>
-            <li><a href="contact.php" title="Contact us">Contact Us</a></li>
-            <li><a href="index.php" title="Return to Home Page">Home Page</a></li>
-        </ul>
-    </section>
 </nav>
