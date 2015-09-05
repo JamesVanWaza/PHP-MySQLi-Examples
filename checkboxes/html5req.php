e17e39540027db736d8f4c1030c6915e99e1b7c9
James Van Waza <jgacuca@gmail.com>
2015-09-05 12:09:52 -0400

    Added the CSS Main File

--- checkboxes/html5req.php
+++ checkboxes/html5req.php
@@ -3,8 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <title>Untitled Document</title>
-<link href="css/main.css" rel="stylesheet" type="text/css">
-
+<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <?php include('nav.php');?>
 
