4cfdd9dac4c745b9c7bf49f9b9cd153997c97a4a
James Van Waza <jgacuca@gmail.com>
2015-09-06 10:58:01 -0400

    Added a list of select options

---
+++ select/select.php
@@ -0,0 +1,17 @@
+<h3>Select your favorite club</h3>
+<form action="check-select.php" method="post">
+<select name="" id="">
+	<option value="">Arsenal</option>
+	<option value="">Aston Villa</option>
+	<option value="">Chelsea</option>
+	<option value="">Liverpool</option>
+	<option value="">Manchester City</option>
+	<option value="">Manchester United</option>
+	<option value="">Norwich</option>
+	<option value="">Paris Saint German</option>
+	<option value="">Real Madrid</option>
+	<option value="">Sevilla</option>
+</select>
+</form>
+<script></script>
+<script></script>
