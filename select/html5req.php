f1fdc32b55de145e627a61421729e7ed4d25c3ef
James Van Waza <jgacuca@gmail.com>
2015-09-06 10:57:38 -0400

    Add the chosen css and js files

---
+++ select/html5req.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+<link rel="stylesheet" type="text/css" href="css/main.css">
+<link rel="stylesheet" type="text/css" href="bower_components/chosen/chosen.min.css">
+</head>
+<?php include('nav.php');?>
