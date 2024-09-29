4113996c19724acaeac053ea9e3e3c4b7d7e50be
JamesVanWaza <jgacuca@gmail.com>
2024-09-29 16:19:40 -0400

    WIP on moving all the files to use the same structure

--- multiple-select/footer.php
+++
@@ -1,5 +0,0 @@
-</body>
-<script type="text/javascript" src="bower_components/jquery/dist/jquery.min.js"></script>
-<script src="bower_components/chosen/chosen.jquery.min.js"></script>
-<script src="js/main.js"></script>
-</html>
--- select/html5req.php
+++
@@ -1,9 +0,0 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Untitled Document</title>
-<link rel="stylesheet" type="text/css" href="css/main.css">
-<link rel="stylesheet" type="text/css" href="bower_components/chosen/chosen.min.css">
-</head>
-<?php include('nav.php');?>
