e6d0b274f4521fccebe0dd71e6bc7b370f0079df
JamesVanWaza <jgacuca@gmail.com>
2024-09-29 15:13:49 -0400

    Updated the URL

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
     <a href="checkboxes/index.php">Checkboxes</a>
     <a href="multiple-select/index.php">Multiple Select</a>
     <a href="radio-buttons/index.php">Radio Buttons</a>
-    <a href="select/index.php">Select</a>
+    <a href="select/select.php">Select</a>
 </body>
 <?php include_once 'footer.php'; ?>
