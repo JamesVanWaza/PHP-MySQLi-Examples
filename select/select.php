58b2f4877ed9ac6f45d1cb57a3756eafb4923389
JamesVanWaza <jgacuca@gmail.com>
2024-09-29 16:13:09 -0400

    Beautified the HTML

--- select/select.php
+++ select/select.php
@@ -1,22 +1,24 @@
-<?php include '../../html5req.php';?>
-<h3>Select your favorite club</h3>
-<form action="check-select.php" method="post">
-<div class="small-12 columns">
-	<select name="" id="chosen-select">
-	<option value="">Arsenal</option>
-	<option value="">Aston Villa</option>
-	<option value="">Chelsea</option>
-	<option value="">Liverpool</option>
-	<option value="">Manchester City</option>
-	<option value="">Manchester United</option>
-	<option value="">Norwich</option>
-	<option value="">Paris Saint German</option>
-	<option value="">Real Madrid</option>
-	<option value="">Sevilla</option>
-</select>
-</div>
-<div class="small-12 columns">
-	<button class="submit-btn">Submit</button>
-</div>
-</form>
-<?php include 'footer.php';?>
+<?php include '../../../html5req.php';?>
+	<h3>Select your favorite club</h3>
+
+	<form action="check-select.php" method="post">
+		<div class="small-12 columns">
+			<select name="" id="chosen-select">
+			<option value="">Arsenal</option>
+			<option value="">Aston Villa</option>
+			<option value="">Chelsea</option>
+			<option value="">Liverpool</option>
+			<option value="">Manchester City</option>
+			<option value="">Manchester United</option>
+			<option value="">Norwich</option>
+			<option value="">Paris Saint German</option>
+			<option value="">Real Madrid</option>
+			<option value="">Sevilla</option>
+		</select>
+		</div>
+		<div class="small-12 columns">
+			<button class="submit-btn">Submit</button>
+		</div>
+	</form>
+	
+<?php include '../../../footer.php';?>
