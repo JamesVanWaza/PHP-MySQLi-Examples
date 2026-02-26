904adf7a34bebe62c40cfe2d5cbcfa20f7dba583
JamesVanWaza <jgacuca@gmail.com>
2026-02-26 15:46:36 -0500

    Created changelog page

---
+++ changelog.php
@@ -0,0 +1,11 @@
+<?php include '../html5req.php'; ?>
+
+<h2>Changelog: 2026.02.02</h2>
+
+<h3>Date: February 26, 2026 15:45</h3>
+
+<ul>
+    <li>Added target="_blank" to all links in checkboxes/index.php</li>
+</ul>
+
+<?php include '../footer.php'; ?>
