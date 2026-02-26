fe8fc3ed2cce43f09242b20df2251709a051bb78
JamesVanWaza <jgacuca@gmail.com>
2026-02-26 15:49:12 -0500

    WIP on setting up the filing structure

--- multiple-select/multiple-checkselect.php
+++ multiple-select/multiple-checkselect.php
@@ -1,48 +1,47 @@
 <?php
-require 'mysqli-connect.php';
-include 'html5req.php';
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	$errors = array(); // Initialize an error array.
+	require 'mysqli-connect.php';
 
-	/*Check whether a hobby has been submitted*/
-	if (empty($_POST['multiple-checkselect'])) {
-		$errors[] = '<p>' . 'You forgot to seelct your options' . '</p>';
+	include 'html5req.php';
+
+	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+		$errors = array(); // Initialize an error array.
+
+		/*Check whether a hobby has been submitted*/
+		if (empty($_POST['multiple-checkselect'])) {
+			$errors[] = '<p>' . 'You forgot to seelct your options' . '</p>';
+		} else {
+			/*The Implode Function is added spaces between the select options*/
+			$hobb = implode(', ', $_POST['multiple-checkselect']);
+		}
+	}
+
+	/*If everything is ok*/
+	if (empty($errors)) {
+		$q = "INSERT INTO multipleselect (id, choices, date_created)
+				VALUES ('id', '$hobb', NOW())";
+		$result = @mysqli_query($dbcon, $q); // Run the query.
+		if ($result) {
+			/*Will only show one hobbie selected
+				$N = count($hobb);
+				echo ("<p>You selected $N Hobbies(s): ");
+				foreach ($errors as $msg) {
+				// Print each error.
+				echo " - $msg<br>\n";
+				}
+				*/
+
+			echo "<div data-alert class='alert-box success radius'>
+				<h3 class='text-center'>The following options have been added</h3>
+				<a href='#' class='close'>&times;</a>
+				</div>";
+		}
 	} else {
-		/*The Implode Function is added spaces between the select options*/
-		$hobb = implode(', ', $_POST['multiple-checkselect']);
+		// echo "<div data-alert class='alert-box alert round'>
+		// 			<h4 class='text-center'>The Hobbies have not been added</h4>
+		// 				<a href='#' class='close'>&times;</a>
+		// 		</div>";
 	}
-}
-/*If everything is ok*/
-if (empty($errors)) {
-	$q = "INSERT INTO multipleselect (id, choices, date_created)
-			VALUES ('id', '$hobb', NOW())";
-	$result = @mysqli_query($dbcon, $q); // Run the query.
-	if ($result) {
-		/*Will only show one hobbie selected
-			$N = count($hobb);
-			echo ("<p>You selected $N Hobbies(s): ");
-			foreach ($errors as $msg) {
-			// Print each error.
-			echo " - $msg<br>\n";
-			}
-			*/
-
-		echo "<div data-alert class='alert-box success radius'>
-			<h3 class='text-center'>The following options have been added</h3>
-			<a href='#' class='close'>&times;</a>
-			</div>";
-	}
-} else {
-	// echo "<div data-alert class='alert-box alert round'>
-	// 			<h4 class='text-center'>The Hobbies have not been added</h4>
-	// 				<a href='#' class='close'>&times;</a>
-	// 		</div>";
-}
-mysqli_close($dbcon);
+	mysqli_close($dbcon);
 ?>
-	<?php include 'footer.php';?>
-	<script src="js/vendor/modernizr.js"></script>
-	<script src="js/foundation.min.js"></script>
-	<script>
-	    $(document).foundation();
-	</script>
+	
+<?php include '../footer.php';?>
