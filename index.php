59053c81861ec233de0935cf5b47cbfd6441822e
JamesVanWaza <jgacuca@gmail.com>
2026-03-14 22:38:17 -0400

    Updated the a link

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <a href="checkboxes/index.php" target="_blank">Checkboxes</a>
     <a href="multiple-select/index.php" target="_blank">Multiple Select</a>
     <a href="radio-buttons/index.php" target="_blank">Radio Buttons</a>
-    <a href="select/select.php" target="_blank">Select</a>
+    <a href="select/index.php" target="_blank">Select</a>
 </body>
 
 <?php include_once 'footer.php'; ?>
