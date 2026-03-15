6812d08ceff61d75a50a8d688f8cf96902cde1e1
JamesVanWaza <jgacuca@gmail.com>
2026-03-14 22:21:33 -0400

    Updated Google Fonts

--- changelog.php
+++ changelog.php
@@ -1,11 +1,22 @@
 <?php include '../html5req.php'; ?>
+    <section class="container">
+    <h2>Changelog: 2026.02.02</h2>
 
-<h2>Changelog: 2026.02.02</h2>
+    <h3>Date: February 26, 2026 15:45</h3>
 
-<h3>Date: February 26, 2026 15:45</h3>
+    <ul>
+        <li>Added target="_blank" to all links in checkboxes/index.php</li>
+    </ul>
+    </section>
 
-<ul>
-    <li>Added target="_blank" to all links in checkboxes/index.php</li>
-</ul>
+    <section class="container">
+    <h2>Changelog: 2026.03.03</h2>
+
+    <h3>Date: March 14, 2026 22:20</h3>
+
+    <ul>
+            <li>Updated font family to use Google Fonts</li>
+    </ul>
+    </section>
 
 <?php include '../footer.php'; ?>
